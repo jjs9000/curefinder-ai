42976b2daaa727b0596347d0b8ae248a1e25096e
jjs9000 <farriszamrie15@gmail.com>
2025-03-16 01:34:10 +0800

    implemented mustverifyemail to new user

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,15 +2,14 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable, HasUuids;
@@ -28,7 +27,8 @@ class User extends Authenticatable
         'email',
         'password',
         'age',
-        'gender'
+        'gender',
+        'github_id',
     ];
 
     /**
