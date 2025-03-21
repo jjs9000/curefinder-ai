cdf4916c6d76a2e09e992a7446e8c5a4bdcb4020
jjs9000 <farriszamrie15@gmail.com>
2025-03-22 00:25:53 +0800

    changed to uuid for chat messages

--- app/Models/ChatMessage.php
+++ app/Models/ChatMessage.php
@@ -3,14 +3,34 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Str;
 
 class ChatMessage extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'user_id',
         'sender',
         'text',
     ];
+
+
+    protected $keyType = 'string'; // Specify that the primary key is a string
+    public $incrementing = false; // Disable auto-incrementing
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        // Automatically generate a UUID for the 'id' field
+        static::creating(function ($model) {
+            if (empty($model->{$model->getKeyName()})) {
+                $model->{$model->getKeyName()} = (string) Str::uuid();
+            }
+        });
+    }
 
     /**
      * Get the user that owns the chat message.
